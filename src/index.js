d8f6f163b33537934733449a85512154d8b72e89
thomassr30 <thomassoto94@gmail.com>
2022-04-17 18:16:38 -0400

    creación de endpoint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 
+const taskRoutes = require('./routes/tasks.routes')
+
 const app = express()
 app.use(morgan('dev'))
+app.use(taskRoutes)
 
 app.listen(4000)
 console.log('server on port 4000')
---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,11 @@
+const {Router} = require('express')
+
+const router = Router();
+
+router.get('/tasks')
+router.get('tasks/:id')
+router.post('/tasks')
+router.delete('/tasks')
+router.put('/tasks')
+
+module.exports = router
