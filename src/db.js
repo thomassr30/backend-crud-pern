c3560757d027ec11a1a93c06a5ab10a76908ed7d
thomassr30 <thomassoto94@gmail.com>
2022-04-17 23:45:30 -0400

    creando variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
-
+const {config} = require('dotenv')
 const {Pool} = require('pg')
-
+config()
 
 const pool = new Pool({
-    user: 'postgres',
+    user: process.env.USER_DB,
     password: process.env.PASSWORD_DB,
-    host: 'localhost',
-    port:5432,
-    database: 'tasksdb'
+    host: process.env.HOST_DB,
+    port:process.env.PORT_DB,
+    database: process.env.DATABASE
 })
 
 module.exports = pool
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const dotenv = require('dotenv/config')
+
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const taskRoutes = require('./routes/tasks.routes')
 
 const app = express()
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(taskRoutes)
